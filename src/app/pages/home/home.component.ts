e8743c6cad038cc763878728cd9fb519d3ad6f09
Vinícius Wagner Gomes Germano <131779346+Vinigg@users.noreply.github.com>
2025-04-03 15:45:02 -0300

    salvando antes de update do angular

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { dataFake } from '../../data/dataFake';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -25,5 +27,23 @@ export class HomeComponent implements OnInit {
   getDescription(id: string): string {
     const article = dataFake.find(article => article.id === id);
     return article ? article.description : '';
+  }
+  ngAfterViewInit() {
+    new Swiper('.vertical-swiper', {
+      direction: 'vertical',
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      freeMode: true,
+      mousewheel: true,
+      modules: [Navigation, Pagination],
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   }
 }
